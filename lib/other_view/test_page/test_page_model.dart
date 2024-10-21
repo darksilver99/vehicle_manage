a71f72c5aaadee8f89771e5421eafda8362fd212
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-21 07:47:09 +0000

    Updating to latest FlutterFlow output.

--- lib/other_view/test_page/test_page_model.dart
+++ lib/other_view/test_page/test_page_model.dart
@@ -4,11 +4,13 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/upload_data.dart';
+import '/rent_view/rent_list_view/rent_list_view_widget.dart';
 import 'test_page_widget.dart' show TestPageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:webviewx_plus/webviewx_plus.dart';
 
 class TestPageModel extends FlutterFlowModel<TestPageWidget> {
   ///  State fields for stateful widgets in this page.
