15b5fa418bf49c669e88440c90e2a4d6c1b96ebf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-12 02:52:16 +0000

    Updating to latest FlutterFlow output.

--- lib/other_view/web_view/web_view_model.dart
+++ lib/other_view/web_view/web_view_model.dart
@@ -1,8 +1,12 @@
+import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_web_view.dart';
+import 'dart:math';
 import 'web_view_widget.dart' show WebViewWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
